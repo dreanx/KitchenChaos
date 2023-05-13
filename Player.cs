53a522cb279d131e8a145337ef0c3e1e489c9989
dreanx <pierre.basck@gmail.com>
2023-05-13 11:08:03 +0200

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Pressing!");
+        }
+        else { Debug.Log("-"); } 
+    }
+}
