a0f41721e1339f907c864ca6321fac676a6b1e61
dreanx <pierre.basck@gmail.com>
2023-05-13 11:28:50 +0200

    Separate the inputs from the movement (converting xy into xz)

--- Player.cs
+++ Player.cs
@@ -27,6 +27,7 @@ public class Player : MonoBehaviour
 
         inputVector = inputVector.normalized;
 
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += (Vector3)inputVector;
 
         Debug.Log(inputVector);
