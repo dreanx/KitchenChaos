d09fbdb48be020c879b49391072c2a7ae60ba4f3
dreanx <pierre.basck@gmail.com>
2023-05-14 14:09:12 +0200

    Added animation to walking

--- Player.cs
+++ Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -32,9 +34,15 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime*rotateSpeed);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
         Debug.Log(inputVector);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking; 
     }
 }
